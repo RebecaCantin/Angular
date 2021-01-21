16f0c38558abdafdf76948711c6d82eba88af2db
rcantinsanchez <rcantinsanchez@deloitte.es>
2021-01-21 16:52:54 +0100

    angular

---
+++ Spring/eclipse-workspace/spring-boot-backend-apires/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andres', 'Guzman', 'andres@gmail.com', '2021-01-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Rebeca', 'Cantin', 'rebeca@gmail.com', '2021-01-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Marcos', 'Corrales', 'marcos@gmail.com', '2021-01-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andrea', 'Gonzalez', 'andrea@gmail.com', '2021-01-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Carlos', 'Diaz', 'carlos@gmail.com', '2021-01-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('David', 'Perez', 'david@gmail.com', '2021-01-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Cesar', 'Cabano', 'cesar@gmail.com', '2021-01-20');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jorge', 'Pardo', 'jorge@gmail.com', '2021-01-20');
